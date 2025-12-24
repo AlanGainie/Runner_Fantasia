46b2eed2b3ad46ada02c9be3125f9f581fa28699
alangainie <alan.gainie@epitech.eu>
2025-12-24 01:55:24 +0100

    udate doxygène

--- src/destroy.c
+++ src/destroy.c
@@ -42,12 +42,22 @@ int destroy(window_t structWindow)
     return 0;
 }
 
+/**
+ * @brief This function destroy all elements permanent used in structWindow
+ * @param structWindow Is a struct used in a body of game
+ * @return int
+ */
 int destroywindows(window_t structWindow)
 {
     destroy(structWindow);
     return 0;
 }
 
+/**
+ * @brief This function destroy all elements permanent used in structWindow
+ * @param structWindow Is a struct used in a body of game
+ * @return int
+ */
 int destroyall(window_t structWindow)
 {
     destroywindows(structWindow);
